23b9aedb2802679a53979216d2424c0d01ed701f
Ars Vladimirov <grokhael@gmail.com>
2023-08-17 00:36:23 +0600

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Industrial Optical Inspection"
 PROJECT_NAME_FULL: str = "Weakly Supervised Learning for Industrial Optical Inspection"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -96,6 +97,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
